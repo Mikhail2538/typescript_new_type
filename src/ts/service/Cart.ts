43ed9ce34e0634096cb9762464f4267a19eca5c8
Mikhail Gr <g_ms@bk.ru>
2025-01-12 19:58:32 +0300

    correction file Cart.ts

--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -1,41 +1,26 @@
 // Интерфейс для описания фильма
 interface IMovie {
-    title: string;
-    year: number;
-    country: string;
-    slogan: string;
-    genre: string[];
-    duration: string; // в формате "137 мин. / 02:17"
+  title: string;
+  year: number;
+  country: string;
+  slogan: string;
+  genre: string[];
+  duration: string; // в формате "137 мин. / 02:17"
 }
 
 // Класс для фильмов
 export class Movie implements IMovie {
-    title: string;
-    year: number;
-    country: string;
-    slogan: string;
-    genre: string[];
-    duration: string;
-
-    constructor(
-        title: string,
-        year: number,
-        country: string,
-        slogan: string,
-        genre: string[],
-        duration: string
-    ) {
-        this.title = title;
-        this.year = year;
-        this.country = country;
-        this.slogan = slogan;
-        this.genre = genre;
-        this.duration = duration;
-    }
-
-    // Метод для отображения информации о фильме
-    displayInfo(): string {
-        return `
+  constructor(
+    public title: string,
+    public year: number,
+    public country: string,
+    public slogan: string,
+    public genre: string[],
+    public duration: string
+  ) {}
+  // Метод для отображения информации о фильме
+  displayInfo(): string {
+    return `
             Название: ${this.title}
             Год: ${this.year}
             Страна: ${this.country}
@@ -43,36 +28,36 @@ export class Movie implements IMovie {
             Жанр: ${this.genre.join(", ")}
             Длительность: ${this.duration}
         `;
-    }
+  }
 }
 
 // Класс корзины для добавления фильмов
 export class Cart {
-    private items: Movie[] = [];
+  private items: Movie[] = [];
 
-    // Добавление фильма в корзину
-    addToCart(movie: Movie): void {
-        this.items.push(movie);
-        console.log(`Фильм "${movie.title}" добавлен в корзину.`);
-    }
+  // Добавление фильма в корзину
+  addToCart(movie: Movie): void {
+    this.items.push(movie);
+    console.log(`Фильм "${movie.title}" добавлен в корзину.`);
+  }
 
-    // Отображение всех фильмов в корзине
-    displayCart(): void {
-        console.log("Корзина содержит следующие фильмы:");
-        this.items.forEach((movie, index) => {
-            console.log(`${index + 1}. ${movie.title}`);
-        });
-    }
+  // Отображение всех фильмов в корзине
+  displayCart(): void {
+    console.log("Корзина содержит следующие фильмы:");
+    this.items.forEach((movie, index) => {
+      console.log(`${index + 1}. ${movie.title}`);
+    });
+  }
 }
 
 // Пример использования
 const avengers = new Movie(
-    "Мстители",
-    2012,
-    "США",
-    "Avengers Assemble!",
-    ["фантастика", "боевик", "фэнтези", "приключения"],
-    "137 мин. / 02:17"
+  "Мстители",
+  2012,
+  "США",
+  "Avengers Assemble!",
+  ["фантастика", "боевик", "фэнтези", "приключения"],
+  "137 мин. / 02:17"
 );
 
 console.log(avengers.displayInfo());
